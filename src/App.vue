c88da1f57fd3a647a7e95f823268e0c6c2adf8d5
karemahamdy <karemahamdy51@gmail.com>
2025-07-14 16:49:02 +0300

    handle excel loader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ExcelExamplePage from './components/ExcelExamplePage.vue';
-import DataTable from './pages/DataTable.vue';
+// import DataTable from './pages/DataTable.vue';
 </script>
 
 <template>
@@ -10,16 +10,4 @@ import DataTable from './pages/DataTable.vue';
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
